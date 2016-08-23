94e1437dc2ce5656a85aab2b43b47f344ad53c7f
Panayiotis Menelaou <panayiotismenelaou@gmail.com>
2016-08-23 17:46:39 +0300

    Add Xbee LCD file in repositroy

---
+++ XBee_LCD/XBee_LCD.ino
@@ -0,0 +1,22 @@
+#include <DanyBotLCD.h>
+DanyBotLCD lcd(0x27, 16, 2);
+void setup() 
+{
+  Serial.begin(9600);
+} 
+
+void loop(){
+  /*
+  lcd.init();  //Initializing the LCD 
+  lcd.backlight();
+  lcd.setCursor(2, 0);  //Setting the Cursorto 3rd column and 0th row
+  lcd.print("Hello World!");  //Pritning the string "Hello World"
+  lcd.setCursor(0, 1);  
+  lcd.print("DanyBot Arduino!");
+  delay(5000);
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("DANY BOT");
+  */
+  
+}
